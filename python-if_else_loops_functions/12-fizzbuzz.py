f1a4bbaa5fc062dd4f9c4e3b53e3aca040a2a0b2
N-Iregi <n.iregi@alustudent.com>
2025-03-12 18:03:43 +0000

    Create 100-print_tebahpla.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,12 +9,10 @@ def fizzbuzz():
     For multiples of five, print Buzz instead of the number.
     For multiples of three and five, print FizzBuzz instead of the number.
     """
-    for number in range(1, 101):
-        if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz ", end="")
-        elif number % 3 == 0:
-            print("Fizz ", end="")
-        elif number % 5 == 0:
-            print("Buzz ", end="")
-        else:
-            print("{} ".format(number), end="")
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3:
+            print("Fizz", end=" ")
+        elif i % 5:
+            print("Buzz", end=" ") 
