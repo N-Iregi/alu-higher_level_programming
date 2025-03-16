3b2929312281c0503e344965788ac4de2af84cc6
N-Iregi <n.iregi@alustudent.com>
2025-03-16 23:16:14 +0000

    Create 2-args.py & 3-infinite_add.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     elif argc > 1:
         print("{} arguments:".format(argc))
 
-        for i, arg in enumerate(argv, start=1):
+        for i, arg in enumerate(argv, start=0):
             print("{}: {}".format(i, arg))
