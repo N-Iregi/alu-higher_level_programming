c067a1732a3cb7bb13912305246a3f53d6a1f9b4
N-Iregi <n.iregi@alustudent.com>
2025-03-18 18:40:53 +0000

    Create 100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,23 +3,22 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    arg_list = sys.argv[1:]
-    len_list = len(arg_list)
+    len_list = len(sys.argv)
 
-    if len_list != 3:
+    if len_list != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = int(arg_list[0])
-    operator = arg_list[1]
-    b = int(arg_list[2])
-    if arg_list[1] == '+':
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    if operator == '+':
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-    elif arg_list[1] == '-':
+    elif operator == '-':
         print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-    elif arg_list[1] == '*':
+    elif operator == '*':
         print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
-    elif arg_list[1] == '/':
+    elif operator == '/':
         print("{} {} {} = {}".format(a, operator, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
