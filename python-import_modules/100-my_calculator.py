74d15ba8b11fb6565e7eb89e701da4cedd0fe521
N-Iregi <n.iregi@alustudent.com>
2025-03-18 18:48:21 +0000

    Create 100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
     len_list = len(sys.argv)
 
     if len_list != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = int(sys.argv[1])
+    try:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+    except ValueError:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    
     operator = sys.argv[2]
-    b = int(sys.argv[3])
     if operator == '+':
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif operator == '-':
