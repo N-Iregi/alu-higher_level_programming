336e0647133b7940f6433d5cd0cbeea9ba6099fc
N-Iregi <n.iregi@alustudent.com>
2025-03-23 15:50:42 +0000

    Create 5-no_c.py && 5-main.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    length = len(my_string)
+    new_string = ''
 
-    j = 0
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
 
-    new_string = my_string[:]
-
-    for i in range(length):
-        if (my_string[i] == 'c' or my_string[i] == 'C'):
-            new_string = new_string[:(i - j)] + my_string[(i + 1):]
-            j += 1
-
-    return (new_string)
+    return new_string
