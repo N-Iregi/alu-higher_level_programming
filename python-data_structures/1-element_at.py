a23749fd1783ea4f4b6c2ab4b8110d051c1a4b49
N-Iregi <n.iregi@alustudent.com>
2025-03-23 08:00:07 +0000

    Create 1-element_at.py & 1-main.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    '''retrieves an element from a list like in C'''
     if idx < 0:
-        return (None)
-
-    length = len(my_list)
-
-    if idx > length - 1:
-        return (None)
-
-    return (my_list[idx])
+        return none
+    elif idx > len(my_list):
+        return none
+    else:
+        return my_list[idx]
