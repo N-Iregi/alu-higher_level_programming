033f947ff22828a48f480e6d5508f5502a21221a
N-Iregi <n.iregi@alustudent.com>
2025-03-24 16:48:57 +0000

    Create 7-add_tuple.py & 7-main.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
     len_b = len(tuple_b)
+    len_a = len(tuple_a)
 
     if len_a == 0:
         a1 = 0
@@ -9,7 +9,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len_a == 1:
         a1 = tuple_a[0]
         a2 = 0
-    else:
+    elif len_a >= 2:
         a1 = tuple_a[0]
         a2 = tuple_a[1]
 
@@ -19,9 +19,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len_b == 1:
         b1 = tuple_b[0]
         b2 = 0
-    else:
+    elif len_b >= 2:
         b1 = tuple_b[0]
         b2 = tuple_b[1]
 
-    new_tuple = (a1 + b1, a2 + b2)
-    return (new_tuple)
+    return (a1 + b1, a2 + b2)
