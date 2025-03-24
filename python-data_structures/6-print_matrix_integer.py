0224ef61246e58d3d23ed02de9dbb7d27e8c5d6a
N-Iregi <n.iregi@alustudent.com>
2025-03-24 14:05:54 +0000

    Create 6-print_matrix_integer.py, 6-main.py & __pycache__/6-print_matrix_integer.cpython-312.pyc

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for index in row:
-            print("{:d}".format(index), end=" ")
-        print()
+        print(" ".join("{:d}".format(j) for j in row))
