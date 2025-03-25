0fff230d2e3d8260cbeb066e54fad49211da9303
N-Iregi <n.iregi@alustudent.com>
2025-03-25 15:12:57 +0000

    Create 10-divisible_by_2.py & 10-main.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    check_div = []
-
+    newlist = my_list[:]
+    
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            check_div.append(True)
+            newlist[i] = True
         else:
-            check_div.append(False)
+            newlist[i] = False
 
-    return (check_div)
+    return newlist
