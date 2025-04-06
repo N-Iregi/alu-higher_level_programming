aaa0cf86e5026eae4d2dfab63a84bc317ad444b4
N-Iregi <n.iregi@alustudent.com>
2025-04-06 14:16:26 +0000

    Create 6-print_sorted_dictionary.py & 6-main.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    list_ord = list(a_dictionary.keys())
-    list_ord.sort()
-    for i in list_ord:
-        print("{}: {}".format(i, a_dictionary.get(i)))
+    list_of_keys = sorted(a_dictionary)
+    for i in range(len(list_of_keys)):
+        print(f"{list_of_keys[i]}: {a_dictionary[list_of_keys[i]]}")
