77aba0b465922c5c1774ff1a3c58d4766d8293c0
N-Iregi <n.iregi@alustudent.com>
2025-04-11 23:18:23 +0000

    Revise questions 12 & 13

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,9 @@ def roman_to_int(roman_string):
     prevvalue = 0
     value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     
-    if not isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str):
+        return 0
+    elif roman_string == None:
         return 0
 
     for c in roman_string:
