6030dce12105669d48e2d9c7189dc4be5c2d536d
N-Iregi <n.iregi@alustudent.com>
2025-04-11 23:13:40 +0000

    Revise questions 12 & 13

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
         return 0
 
-    num = 0
-    den = 0
+    total_weight_score = 0
+    total_weight = 0
 
     for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
+        total_weight_score += tup[0]*tup[1]
+        total_weight += tup[1]
 
-    return (num / den)
+    average_weight = total_weight_score/total_weight
+    return average_weight
