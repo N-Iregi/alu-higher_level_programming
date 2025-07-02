870965a3726eea42f233182e5266e8b74979bd01
N-Iregi <n.iregi@alustudent.com>
2025-07-02 19:57:19 +0000

    Modify 2-delete.sh

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends DELETE request to given URL and displays body of response
-curl -sX DELETE "$1"
+# sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -s -X DELETE "$1"
