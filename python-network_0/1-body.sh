e6d79b8a0a4588da1c1fcb6034280753d2e25a92
N-Iregi <n.iregi@alustudent.com>
2025-07-02 19:41:28 +0000

    Create files

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sfL "$1" -X GET
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+r=$(curl -s -L -w "%{http_code}" "$1"); s=${r: -3}; [[ $s == 200 ]] && echo -n "${r:: -3}"
