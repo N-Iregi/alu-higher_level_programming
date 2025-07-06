69736a113b22061bcb1c8a4401e55992c2544165
N-Iregi <n.iregi@alustudent.com>
2025-07-06 15:43:30 +0000

    Create File 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alu-intranet.hbtn.io/status.
-- uses urlib package
-"""
+# fetches https://alu-intranet.hbtn.io/status using urllib
 
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
-if url.startswith('https://'):
-    url = 'https://alu-intranet.hbtn.io/status'
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+        fetch_page = response.read()
 
-if __name__ == '__main__':
-    with urllib.request.urlopen(url) as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    print("Body Response:")
+    print("\t- type:", type(fetch_page))
+    print("\t- content:", fetch_page)
+    print("\t- utf8 content:", fetch_page.decode('utf-8'))
--- python-network_1/README.md
+++ python-network_1/README.md
@@ -1,1 +1,12 @@
+This is a project on learning how to navigate the network using Python(urllib & requests packages).
+Some of the learning objectives include:
 
+	1. How to fetch internet resources with the Python package urllib
+	2. How to decode urllib body response
+	3. How to use the Python package requests #requestsiswaysimplerthanurllib
+	4. How to make HTTP GET request
+	5. How to make HTTP POST/PUT/etc. request
+	6. How to fetch JSON resources
+	7. How to manipulate data from an external service
+
+The tasks in this project are scripts that aim to meet this learning objectives.
