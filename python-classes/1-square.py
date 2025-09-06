62eb2199b8661666eb036af67135aabd20317e0c
N-Iregi <n.iregi@alustudent.com>
2025-09-06 09:00:04 +0000

    Revise 2-square.py and add 2-main.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Write a class Square that defines a square by"""
-
+"""Creates a class Square that defines a square"""
 
 class Square:
     """Private instance attribute: size
-    Instantiation with size (no type/value verification)"""
+    Instantiation with size (no type/value verification)
+	"""
     def __init__(self, size):
         self.__size = size
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,13 +5,11 @@
 class Square:
     """write a class that defines a square"""
     def __init__(self, size=0):
-        """initialize square
-               Args:
-                   size (int): size of the square
-               """
-        if type(size) is not int:
+        """initialize square with args:
+        1. size (int): size of the square
+        """
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise TypeError("size must be >= 0")
-        else:
-            self.__size = size
+             raise ValueError("size must be >= 0")
+        self.__size = size
